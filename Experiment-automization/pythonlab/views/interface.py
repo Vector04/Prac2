aa9ec3296596b0f9743903cd6d5a2bb26c8fb0b9
Victor Vreede <victor@vreede.eu>
2020-12-06 16:37:22 +0100

    Finished interface

--- Experiment-automization/pythonlab/views/interface.py
+++ Experiment-automization/pythonlab/views/interface.py
@@ -1,5 +1,5 @@
 import sys
-import threading
+import logging
 
 import numpy as np
 import pandas as pd
@@ -13,13 +13,25 @@ from pythonlab.models.models import DiodeExperiment as DE
 pg.setConfigOption("background", "w")
 pg.setConfigOption("foreground", "k")
 
+# Logging, only by the root.
+for logger in logging.Logger.manager.loggerDict:
+    logging.getLogger(logger).disabled = True
+
 
 class UserInterface(QtWidgets.QMainWindow):
-    def __init__(self, debug=False):
+    """Controls functionality of our QApplication. implements logic that enables the features of the user interface.
+        Features are:
+            - Port scanning and selection
+            - Port verification
+            - Scanning voltage range adjustment
+            - Live result display with errorbars
+            - Data export capabilities.
+    """
+
+    def __init__(self):
         """Creates a UserInterface() instance, enables interactive functionality of ui."""
 
         super().__init__()
-        self.debug = debug
         self.resource = None
 
         uic.loadUi(pkg_resources.resource_stream(
@@ -68,33 +80,34 @@ class UserInterface(QtWidgets.QMainWindow):
         """Sets default resource (port), displays it"""
         self.resource = self.resource_select.currentText()
         self.selected_device_line_edit.setText(self.resource)
+        logging.info(f"Device {self.resource} set.")
 
     def get_identification(self):
         """Gets the identification string of device, and displays it."""
         self.identification_line_edit.setText('')
         resource = self.resource_select.currentText()
-        if self.debug:
-            print(resource)
+        logging.debug(f"Getting Identification for {resource}.")
 
         # Unfortunately, this is very slow
         info = DE.get_info(resource)
-        if self.debug:
-            print(info)
 
         if info:
             self.identification_line_edit.setText(info)
+            logging.info(f"Identification for {resource} is {info}.")
         else:
             self.identification_line_edit.setText("Not Found!")
+            logging.warning(f"No Identification found for {resource}.")
 
     def file_select(self):
         """Opens File Dialog box to select file. Displays path/to/file as well."""
+        logging.debug(f"Propting to get filename.")
         self.filename, _ = QtWidgets.QFileDialog.getSaveFileName(
             filter="CSV files(*.csv)")
         self.filename_line_edit.setText(self.filename)
+        logging.debug(f"Filename is {self.filename}.")
+
         # Useful, see the save_data method
         return self.filename
-        if self.debug:
-            print(self.filename)
 
     def save_data(self):
         """Saves measured data to csv. If there is no data, no file is saved."""
@@ -102,23 +115,20 @@ class UserInterface(QtWidgets.QMainWindow):
             df = pd.DataFrame({"v_in": self.xs, "v_out": self.ys})
         except AttributeError:
             # There is no data to be saved
-            if self.debug:
-                print("Tried to save, but no data!")
+            logging.debug("There is no data to be saved.")
             return
         else:
             try:
                 df.to_csv(self.filename, index=False)
             except AttributeError:
                 # self.filename is not defined, ask again
-                if self.debug:
-                    print("No filename found, promting FileDialog!")
+                logging.debug("No filename found, promting a FileDialog.")
                 if self.file_select():
                     self.save_data()
             else:
                 # Give user feedback that data was saved succesfully
                 self.savebutton.setStyleSheet("background-color: #76fa34")
-                if self.debug:
-                    print("Save succesful!")
+                logging.info("Data saved succesfully!")
 
     def start_measurement(self):
         """Evokes pythonlab.models module to measure data over specified range, plots data was well. Has spam protection."""
@@ -143,10 +153,9 @@ class UserInterface(QtWidgets.QMainWindow):
         self.savebutton.setStyleSheet("")
         self.plot_timer.start(100)
 
-        if self.debug:
-            print(args)
+        logging.debug(f"Scan parameters: {args}.")
 
-        # Note: This was where I had trouble with threading, I implementen a better and more elegant solution.
+        # Note: This was where I had trouble with threading, I implemented a better and more elegant solution.
         # Instantiating data collection thread
         self.data_thread = DataCollectionThread(*args, port=self.resource)
 
@@ -156,23 +165,33 @@ class UserInterface(QtWidgets.QMainWindow):
         self.data_thread.finished.connect(self.end_measurement)
 
         self.data_thread.start()
+        logging.info(f"Starting data Scan.")
 
     def end_measurement(self):
         """Activates miscellaneous functions to properly finish the measurement."""
         self.measure_button.setEnabled(True)
         self.plot_timer.stop()
         self.is_plotting = False
+        logging.info(f"Finished data scan.")
 
-    def _plot(self, ys):
+    def _plot(self, ys, dys):
         """Plots the given data on plotwidget instance.
         Arguments:
             `ys': list, the data to plot. 
+            `dys': list, the uncertainties over the measurement.
         """
+        logging.debug("_plot() activated.")
         self.plotwidget.clear()
         self.plotwidget.plot(self.xs[:len(ys)], ys, **self.plotting_options)
+        errorbars = pg.ErrorBarItem(
+            x=self.xs[:len(ys)],
+            y=ys,
+            width=2 * np.array(dys),
+            height=2 * np.array(dys))
+        self.plotwidget.addItem(errorbars)
 
-    def copy_ys(self, ys):
-        """Copies data from DatacollectionThread to Userinterface.ys attribute.
+    def copy_ys(self, ys, dys):
+        """Copies data from DataCollectionThread to Userinterface.ys attribute.
         Arguments:
             `ys': list, the data to be copied. 
         """
@@ -180,13 +199,14 @@ class UserInterface(QtWidgets.QMainWindow):
 
 
 class DataCollectionThread(QtCore.QThread):
-    ys_signal = QtCore.pyqtSignal(list)
+    ys_signal = QtCore.pyqtSignal(list, list)
 
     def __init__(self, *args, **kwargs):
         QtCore.QThread.__init__(self)
         self.args = args
         self.kwargs = kwargs
         self.ys = []
+        self.dys = []
 
     def __del__(self):
         self.wait()
@@ -194,20 +214,26 @@ class DataCollectionThread(QtCore.QThread):
     def run(self):
         for V_out in DE.get_voltages(*self.args, **self.kwargs):
             self.ys.append(V_out)
-            self.ys_signal.emit(self.ys)
+            self.dys.append(0.0033)
+            self.ys_signal.emit(self.ys, self.dys)
+            logging.debug(f"New datapoint [{V_out}] added.")
 
-# Click command
 
-
-@ click.group()
+@click.group()
 def cli():
     pass
 
 
-@ cli.command()
-def run():
+@cli.command()
+@click.option("-log", default='info', type=str, help="The logging level.")
+def run(log='debug'):
+    """Runs the application. 
+    Option `log' represents the logging level. It can be left blank, or set to one of the following:
+    debug, info, warning, error, critical.
+     """
+    logging.basicConfig(level=getattr(logging, log.upper()))
     app = QtWidgets.QApplication(sys.argv)
-    ui = UserInterface(debug=True)
+    ui = UserInterface()
     ui.show()
     sys.exit(app.exec())
 
